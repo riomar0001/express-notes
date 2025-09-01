c60e9571b1946b00d9df4b38e0671a3caead9f1d
itsmejastine <152354420+itsmejastine@users.noreply.github.com>
2025-09-01 16:35:13 +0800

    Update notes.controller.js

---
+++ src/controller/notes.controller.js
@@ -0,0 +1,27 @@
+import { Prisma } from "../config/prisma.config.js";
+
+export const createNote = async (req, res) => {
+    try {
+        const { name , description } = req.body;
+
+        if (!name || !description) {
+            res
+            .status(400)
+            .send({ success: false, message: "Please enter all fields"):
+            }
+
+            const createNote = await Prisma.notes.create({
+                data: {
+                    name,
+                    description,
+                },
+            });
+
+            return res.status(200).send({ success: true, data: createNote});
+    }catch (error){
+        return res.status(500).send({ success: false, message: error.message});
+
+    }
+
+        
+};
