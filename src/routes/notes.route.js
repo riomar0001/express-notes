1dee9002930b5b65a95488c7e4be25accc795476
Riekity <marie.lomarda07@gmail.com>
2025-09-02 13:17:22 +0800

    missing requests is added

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { createNote, viewNote } from "../controller/notes.controller.js";
+import { createNote, delNote, getNotes, uptNote, viewNote } from "../controller/notes.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createNote);
-router.get("/view/:id", createNote);
+router.get("/view/:id", viewNote);
+router.get("/viewAll", getNotes);
+router.put("/update/:id", uptNote);
+router.delete("/delete/:id", delNote);
+
 
 export default router;
