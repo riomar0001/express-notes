32bce76a574c4c696045013fea7744bae820ad76
mnlpatayan111800-commits <mnl.patayan111800@gmail.com>
2025-09-02 13:49:27 +0800

    Added all the missing request

--- src/controller/notes.controller.js
+++ src/controller/notes.controller.js
@@ -56,3 +56,85 @@ export const viewNote = async (req, res) => {
     return res.status(500).send({ success: false, message: error.message });
   }
 };
+
+export const viewAllNotes = async (req, res) => {
+  try {
+    const notes = await Prisma.notes.findMany({
+      orderBy: {
+        create_at: "desc",
+      },
+    });
+
+    return res.status(200).send({ success: true, data: notes });
+  } catch (error) {
+    return res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const { name, description } = req.body;
+
+    if (!noteId) {
+      return res.status(400).send({
+        success: false,
+        message: "Note ID is required",
+      });
+    }
+
+    const updatedNote = await Prisma.notes.update({
+      where: { id: noteId },
+      data: {
+        name,
+        body: description,
+        update_at: new Date(),
+      },
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Note updated successfully",
+      data: updatedNote,
+    });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).send({
+        success: false,
+        message: "Note not found",
+      });
+    }
+    return res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+
+export const deleteNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+
+    if (!noteId) {
+      return res.status(400).send({
+        success: false,
+        message: "Note ID is required",
+      });
+    }
+
+    await Prisma.notes.delete({
+      where: { id: noteId },
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Note deleted successfully",
+    });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).send({
+        success: false,
+        message: "Note not found",
+      });
+    }
+    return res.status(500).send({ success: false, message: error.message });
+  }
+};
--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { createNote, viewNote } from "../controller/notes.controller.js";
+import { createNote, deleteNote, update, viewAllNotes, viewNote } from "../controller/notes.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createNote);
-router.get("/view/:id", createNote);
+router.get("/view/:id", viewNote);
+router.get("/viewAll", viewAllNotes);
+router.put("/update/:id", update);
+router.delete("/delete/:id", deleteNote);
 
 export default router;
